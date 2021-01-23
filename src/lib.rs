eeff6c96a219eadd59432c111a7496d5cce6a867
Andrew Jones <ascjones@gmail.com>
2021-01-23 20:14:43 +0000

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+// Copyright 2018-2020 Parity Technologies (UK) Ltd.
+// This file is part of scale-dynamic.
+//
+// cargo-contract is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// cargo-contract is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with cargo-contract.  If not, see <http://www.gnu.org/licenses/>.
+
+pub mod scon;
+mod transcoder;
+
+pub use self::{
+    transcoder::Transcoder,
+};
+
+#[derive(thiserror::Error, Debug, PartialEq)]
+pub enum Error {
+    #[error("Struct fields should either be all named or all unnamed")]
+    InvalidCompositeTypeFields
+}
+
